df89e062f854b7fb11495994064518145a86de64
Mainfrezzer <35963833+Mainfrezzer@users.noreply.github.com>
2025-06-29 17:53:08 +0200

    change of url

--- build/06-cron.sh
+++ build/06-cron.sh
@@ -1,6 +1,6 @@
 cat<<EOF>/etc/periodic/weekly/roothint
 #!/bin/sh
-/usr/bin/wget -S https://www.internic.net/domain/named.cache -O /etc/unbound/root.hints >/dev/null 2>&1
+/usr/bin/wget -S https://www.internic.net/domain/named.root -O /etc/unbound/root.hints >/dev/null 2>&1
 rc-service unbound restart
 /opt/AdGuardHome/AdGuardHome --update
 #/root/update-lancache.sh > /var/log/update-lancache.log
